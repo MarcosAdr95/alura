bcbf5fe0afa8778856dd88dcb1bb282af9d901ab
Marcos Adriano <mamelo@band.com.br>
2025-07-09 16:04:53 -0300

    wp-config set disable_file_edit-false

--- wp-config.php
+++ wp-config.php
@@ -93,6 +93,10 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
+// Enable wordpress file's editor
+define('DISALLOW_FILE_EDIT', true);
+
+
 /* Add any custom values between this line and the "stop editing" line. */
 
 
